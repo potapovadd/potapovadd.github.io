c8dddc370c0c8560942b5e974c343ccded6673b4
potapovadd <144013043+potapovadd@users.noreply.github.com>
2023-10-22 22:40:42 +0300

    Add files via upload

--- 6/calculator.js
+++ 6/calculator.js
@@ -1,21 +1,21 @@
 const number = document.getElementById("field1");
 const radio = document.querySelectorAll('input[name="myradio"]'); 
-const select = document.getElementById('select');
-const option = document.getElementById('select1');
-const checkbox = document.getElementById('checkbox');
-const check1 = document.getElementsByName('check-1');
-const result = document.getElementById('result');
+const select = document.getElementById("select");
+const option = document.getElementById("select1");
+const checkbox = document.getElementById("checkbox");
+const check1 = document.getElementsByName("check-1");
+const result = document.getElementById("result");
 
 window.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed");
-    number.addEventListener('input', Summa);
+    number.addEventListener("input", Summa);
     radio.forEach((input) => {
-        input.addEventListener('change', () => {
+        input.addEventListener("change", () => {
             Summa();
             Show();
         });
 });
-    option.addEventListener('change', Summa);
+    option.addEventListener("change", Summa);
 });
 
 function Summa() {
@@ -24,11 +24,11 @@ function Summa() {
     let selValue = 0;
     let checkValue = 0;
 
-    r1 = document.querySelector('input[name="myradio"]:checked').value;
-    if (r1 === '500') {
+    let r1 = document.querySelector('input[name="myradio"]:checked').value;
+    if (r1 === "500") {
         selValue = parseInt(option.value);
     }
-    else if (r1 === '300') {
+    else if (r1 === "300") {
         if (check1[0].checked) {
             checkValue = 100;
         }
@@ -37,18 +37,19 @@ function Summa() {
 }
 
 function Show() {
-    r1 = document.querySelector('input[name="myradio"]:checked').value;
-    if (r1 === '1000') {
-        select.style.display = 'none';
-        checkbox.style.display = 'none';
+    let r1 = document.querySelector('input[name="myradio"]:checked').value;
+    if (r1 === "1000") {
+        select.style.display = "none";
+        checkbox.style.display = "none";
     } 
-    else if (r1 === '500') {
-        select.style.display = 'block';
-        checkbox.style.display = 'none';
+    else if (r1 === "500") {
+        select.style.display = "block";
+        checkbox.style.display = "none";
     } 
-    else if (r1 === '300') {
-        select.style.display = 'none';
-        checkbox.style.display = 'block';
+    else if (r1 === "300") {
+        select.style.display = "none";
+        checkbox.style.display = "block";
     }
     Summa();
 }
+
